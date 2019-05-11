79bed2860af5db72cf0dcba9723b6ab5f39c6e8f
khanhas <xuankhanh963@gmail.com>
2019-05-11 11:09:11 +1000

    change: simpler progress Tracker

--- src/utils/tracker.go
+++ src/utils/tracker.go
@@ -1,50 +1,42 @@
 package utils
 
 import (
-	"io/ioutil"
-
-	pb "gopkg.in/cheggaaa/pb.v2"
+	"fmt"
+	"log"
+	"strings"
 )
 
-type tracker struct {
-	instance *pb.ProgressBar
+type Tracker struct {
+	current  int
 	total    int
-}
-
-// Tracker is a counter
-type Tracker interface {
-	Update(name string, err error)
-	Finish()
-	Reset()
-	Quiet()
+	maxLen   int
 }
 
 // NewTracker creates new tracker instance
-func NewTracker(total int) Tracker {
-	return tracker{
-		instance: pb.ProgressBarTemplate(`[{{counters . }}] {{string . "name"}}`).Start(total),
-		total:    total,
+func NewTracker(total int) *Tracker {
+	t := &Tracker{ 0, total, 0 }
+	return t
+}
+
+func (t *Tracker) Update(name string) {
+	t.current++
+	line := fmt.Sprintf("\r[ %d / %d ] %s", t.current, t.total, name)
+	lineLen := len(line)
+	spaceLen := 0
+
+	if lineLen > t.maxLen {
+		t.maxLen = lineLen
+	} else {
+		spaceLen = t.maxLen - lineLen
 	}
+
+	fmt.Print(line + strings.Repeat(" ", spaceLen))
 }
 
-func (t tracker) Update(name string, err error) {
-	t.instance.Increment()
-	t.instance.Set("name", name)
-	if err != nil {
-		PrintError(err.Error())
-	}
+func (t *Tracker) Finish() {
+	log.Println("\r\x1B[32mOK\033[0m" + strings.Repeat(" ", t.maxLen - 2))
 }
 
-func (t tracker) Finish() {
-	t.instance.Set("name", "\x1B[32mOK\033[0m")
-	t.instance.Finish()
-}
-
-func (t tracker) Reset() {
-	t.instance.SetCurrent(0)
-	t.instance.Start()
-}
-
-func (t tracker) Quiet() {
-	t.instance.SetWriter(ioutil.Discard)
+func (t *Tracker) Reset() {
+	t.current = 0
 }
