d88231b9022ce03337190efe7dd61c19a7c6c8d7
khanhas <xuankhanh963@gmail.com>
2019-06-17 16:52:55 +1000

    refactor: utils Print functions

--- src/utils/print.go
+++ src/utils/print.go
@@ -5,39 +5,29 @@ import (
 	"os"
 )
 
-// PrintWarning prints a warning message
-func PrintWarning(text string) {
-	log.Println("\x1B[33mwarning\033[0m", text)
-}
-
-// PrintError prints an error message
-func PrintError(text string) {
-	log.Println("\x1B[31merror\033[0m", text)
-}
-
-// PrintSuccess prints a success message
-func PrintSuccess(text string) {
-	log.Println("\x1B[32msuccess\033[0m", text)
-}
-
-// PrintInfo prints an info message
-func PrintInfo(text string) {
-	log.Println("\x1B[34minfo\033[0m", text)
-}
-
-// PrintGreen prints a message in green color
-func PrintGreen(text string) {
-	log.Println("\x1B[32m" + text + "\033[0m")
-}
-
-// PrintRed prints a red message
-func PrintRed(text string) {
-	log.Println("\x1B[31m" + text + "\033[0m")
-}
-
 // Bold .
 func Bold(text string) string {
 	return "\x1B[1m" + text + "\033[0m"
+}
+
+// Red .
+func Red(text string) string {
+	return "\x1B[31m" + text + "\x1B[0m"
+}
+
+// Green .
+func Green(text string) string {
+	return "\x1B[32m" + text + "\x1B[0m"
+}
+
+// Yellow .
+func Yellow(text string) string {
+	return "\x1B[33m" + text + "\x1B[0m"
+}
+
+// Blue .
+func Blue(text string) string {
+	return "\x1B[34m" + text + "\x1B[0m"
 }
 
 // PrintBold prints a bold message
@@ -45,8 +35,38 @@ func PrintBold(text string) {
 	log.Println(Bold(text))
 }
 
+// PrintRed prints a message in red color
+func PrintRed(text string) {
+	log.Println(Red(text))
+}
+
+// PrintGreen prints a message in green color
+func PrintGreen(text string) {
+	log.Println(Green(text))
+}
+
+// PrintWarning prints a warning message
+func PrintWarning(text string) {
+	log.Println(Yellow("warning"), text)
+}
+
+// PrintError prints an error message
+func PrintError(text string) {
+	log.Println(Red("error"), text)
+}
+
+// PrintSuccess prints a success message
+func PrintSuccess(text string) {
+	log.Println(Green("success"), text)
+}
+
+// PrintInfo prints an info message
+func PrintInfo(text string) {
+	log.Println(Blue("info"), text)
+}
+
 // Fatal prints fatal message and exits process
 func Fatal(err error) {
-	log.Println("\x1B[31mfatal\033[0m", err)
+	log.Println(Red("fatal"), err)
 	os.Exit(1)
 }
