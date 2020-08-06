2df6e68c4f5579ce709785420aa0b0790bc9ddf8
Khánh Nguyễn Xuân <xuankhanh963@gmail.com>
2020-08-06 23:24:35 +1000

    cleanup:

--- src/cmd/upgrade.go
+++ src/cmd/upgrade.go
@@ -9,88 +9,19 @@ import (
 	"os/exec"
 	"path/filepath"
 	"runtime"
-	"time"
 
 	"github.com/khanhas/spicetify-cli/src/utils"
 )
 
 type githubRelease struct {
-	URL             string `json:"url"`
-	AssetsURL       string `json:"assets_url"`
-	UploadURL       string `json:"upload_url"`
-	HTMLURL         string `json:"html_url"`
-	ID              int    `json:"id"`
-	NodeID          string `json:"node_id"`
-	TagName         string `json:"tag_name"`
-	TargetCommitish string `json:"target_commitish"`
-	Name            string `json:"name"`
-	Draft           bool   `json:"draft"`
-	Author          struct {
-		Login             string `json:"login"`
-		ID                int    `json:"id"`
-		NodeID            string `json:"node_id"`
-		AvatarURL         string `json:"avatar_url"`
-		GravatarID        string `json:"gravatar_id"`
-		URL               string `json:"url"`
-		HTMLURL           string `json:"html_url"`
-		FollowersURL      string `json:"followers_url"`
-		FollowingURL      string `json:"following_url"`
-		GistsURL          string `json:"gists_url"`
-		StarredURL        string `json:"starred_url"`
-		SubscriptionsURL  string `json:"subscriptions_url"`
-		OrganizationsURL  string `json:"organizations_url"`
-		ReposURL          string `json:"repos_url"`
-		EventsURL         string `json:"events_url"`
-		ReceivedEventsURL string `json:"received_events_url"`
-		Type              string `json:"type"`
-		SiteAdmin         bool   `json:"site_admin"`
-	} `json:"author"`
-	Prerelease  bool      `json:"prerelease"`
-	CreatedAt   time.Time `json:"created_at"`
-	PublishedAt time.Time `json:"published_at"`
-	Assets      []struct {
-		URL      string      `json:"url"`
-		ID       int         `json:"id"`
-		NodeID   string      `json:"node_id"`
-		Name     string      `json:"name"`
-		Label    interface{} `json:"label"`
-		Uploader struct {
-			Login             string `json:"login"`
-			ID                int    `json:"id"`
-			NodeID            string `json:"node_id"`
-			AvatarURL         string `json:"avatar_url"`
-			GravatarID        string `json:"gravatar_id"`
-			URL               string `json:"url"`
-			HTMLURL           string `json:"html_url"`
-			FollowersURL      string `json:"followers_url"`
-			FollowingURL      string `json:"following_url"`
-			GistsURL          string `json:"gists_url"`
-			StarredURL        string `json:"starred_url"`
-			SubscriptionsURL  string `json:"subscriptions_url"`
-			OrganizationsURL  string `json:"organizations_url"`
-			ReposURL          string `json:"repos_url"`
-			EventsURL         string `json:"events_url"`
-			ReceivedEventsURL string `json:"received_events_url"`
-			Type              string `json:"type"`
-			SiteAdmin         bool   `json:"site_admin"`
-		} `json:"uploader"`
-		ContentType        string    `json:"content_type"`
-		State              string    `json:"state"`
-		Size               int       `json:"size"`
-		DownloadCount      int       `json:"download_count"`
-		CreatedAt          time.Time `json:"created_at"`
-		UpdatedAt          time.Time `json:"updated_at"`
-		BrowserDownloadURL string    `json:"browser_download_url"`
-	} `json:"assets"`
-	TarballURL string `json:"tarball_url"`
-	ZipballURL string `json:"zipball_url"`
-	Body       string `json:"body"`
+	TagName string `json:"tag_name"`
 }
 
 func Upgrade(currentVersion string) {
 	utils.PrintBold("Fetch latest release info:")
-    tagName := FetchLatestTag()
+	tagName := FetchLatestTag()
 	utils.PrintGreen("OK")
+
 	utils.PrintInfo("Current version: " + currentVersion)
 	utils.PrintInfo("Latest release: " + tagName)
 	if currentVersion == tagName {
@@ -184,5 +115,6 @@ func FetchLatestTag() string {
 	if err = json.Unmarshal(body, &release); err != nil {
 		utils.Fatal(err)
 	}
-    return release.TagName[1:]
+
+	return release.TagName[1:]
 }
