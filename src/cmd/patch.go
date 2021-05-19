c0da78d3d8a427dc9e3cd049ca9875fdbf88e088
khanhas <xuankhanh963@gmail.com>
2021-05-19 11:28:31 +1000

    add: Patch feature functional

--- src/cmd/patch.go
+++ src/cmd/patch.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"strings"
 
 	"github.com/khanhas/spicetify-cli/src/utils"
 )
@@ -11,7 +12,7 @@ import (
 func Patch() {
 	keys := patchSection.Keys()
 
-	re := regexp.MustCompile(`([\w\-]+)_find_(\d+)`)
+	re := regexp.MustCompile(`^([\w\d\-\.]+)_find_(\d+)$`)
 	for _, key := range keys {
 		keyName := key.Name()
 		matches := re.FindStringSubmatch(keyName)
@@ -20,36 +21,44 @@ func Patch() {
 		}
 
 		name := matches[1]
-		pathToApp := filepath.Join(appPath, name)
+		assetPath := filepath.Join(appPath, "xpui", name)
 		index := matches[2]
 
-		if _, err := os.Stat(pathToApp); err != nil {
-			utils.PrintError("App name \"" + name + "\" is not valid.")
+		if _, err := os.Stat(assetPath); err != nil {
+			utils.PrintError("File name \"" + name + "\" is not found.")
 			continue
 		}
 
-		replName := name + "_repl_" + index
-		replKey, err := patchSection.GetKey(replName)
-		if err != nil {
+		replName := name + "_repl_all_" + index
+		replOnceName := name + "_repl_" + index
+		replKey, errAll := patchSection.GetKey(replName)
+		replOnceKey, errOnce := patchSection.GetKey(replOnceName)
+
+		if errAll != nil && errOnce != nil {
 			utils.PrintError("Cannot find replace string for patch \"" + keyName + "\"")
-			utils.PrintInfo("Correct key name for replace string is \"" + replName + "\"")
+			utils.PrintInfo("Correct key name for replace string are")
+			utils.PrintInfo("    \"" + replOnceName + "\"")
+			utils.PrintInfo("    \"" + replName + "\"")
 			continue
 		}
 
-		patchRegexp, err := regexp.Compile(key.String())
-		if err != nil {
+		patchRegexp, errReg := regexp.Compile(key.String())
+		if errReg != nil {
 			utils.PrintError("Cannot compile find RegExp for patch \"" + keyName + "\"")
 			continue
 		}
 
-		jsFilePath := filepath.Join(pathToApp, name+".bundle.js")
-		if _, err := os.Stat(jsFilePath); err != nil {
-			utils.PrintError("App javascript file \"" + jsFilePath + "\" not found.")
-			continue
-		}
-
-		utils.ModifyFile(jsFilePath, func(content string) string {
-			return patchRegexp.ReplaceAllString(content, replKey.MustString(""))
+		utils.ModifyFile(assetPath, func(content string) string {
+			if errAll == nil { // Priotize replace all
+				return patchRegexp.ReplaceAllString(content, replKey.MustString(""))
+			} else {
+				match := patchRegexp.FindString(content)
+				if len(match) > 0 {
+					toReplace := patchRegexp.ReplaceAllString(match, replOnceKey.MustString(""))
+					content = strings.Replace(content, match, toReplace, 1)
+				}
+				return content
+			}
 		})
 
 		utils.PrintSuccess("\"" + keyName + "\" is patched")
