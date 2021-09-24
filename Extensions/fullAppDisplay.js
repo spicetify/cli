44d7d020bfdf00b8c2ed177a756ad8549517050f
itsmeow <mods.itsmeow@gmail.com>
2021-09-24 03:32:41 -0500

    Fix issues with 2.6.5 (#1093)
    * Fix full app display using lots of CPU
    
    * Fix Lyrics Plus starting app with no lyrics

--- Extensions/fullAppDisplay.js
+++ Extensions/fullAppDisplay.js
@@ -484,10 +484,10 @@ body.video-full-screen.video-full-screen--hide-ui {
         requestAnimationFrame(animate);
     }
 
-    function updateProgress() {
-        prog.style.width = Spicetify.Player.getProgressPercent() * 100 + "%";
+    function updateProgress(event) {
+        prog.style.width = (event.data / Spicetify.Player.origin._state.duration) * 100 + "%";
         elaps.innerText = Spicetify.Player.formatTime(
-            Spicetify.Player.getProgress()
+            event.data
         );
     }
 
@@ -503,7 +503,6 @@ body.video-full-screen.video-full-screen--hide-ui {
         updateInfo();
         Spicetify.Player.addEventListener("songchange", updateInfo);
         if (CONFIG.enableProgress) {
-            updateProgress();
             Spicetify.Player.addEventListener("onprogress", updateProgress);
         }
         if (CONFIG.enableControl) {
