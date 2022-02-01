c67e99fb032128453409859c6b6260b4f98494b0
Afonso Ramos <afonsojorgeramos@gmail.com>
2022-02-01 01:38:41 +0000

    fix: rollback `fish` change

--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ path="export PATH=\"\$PATH:\$HOME/.spicetify\""
 
 if [[ grep -q "bash" $SHELL ]] && ! grep -q "$path" "$HOME/.bashrc"; then echo "${path}" >>"$HOME/.bashrc" && echo "SAVING         ${spicetify_install} to \$PATH (bash)"; fi
 if [[ grep -q "zsh" $SHELL ]] && ! grep -q "$path" "$HOME/.zshrc"; then echo "${path}" >>"$HOME/.zshrc" && echo "SAVING         ${spicetify_install} to \$PATH (zsh)"; fi
-if [[ grep -q "fish" $SHELL ]] && ! grep -q "$path" "$HOME/.config/fish/config.fish"; then echo "${path}" >>"$HOME/.config/fish/config.fish" && echo "SAVING         ${spicetify_install} to \$PATH (fish)"; fi
+if [[ -f "$HOME/.config/fish/config.fish" ]] && ! grep -q "$path" "$HOME/.config/fish/config.fish"; then echo "${path}" >>"$HOME/.config/fish/config.fish" && echo "SAVING         ${spicetify_install} to \$PATH (fish)"; fi
 
 exe="$spicetify_install/spicetify"
 
