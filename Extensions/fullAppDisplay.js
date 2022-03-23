af9640ffeded91b4233fb0af520c2bb769b6e764
huhridge <sharmaharij21@gmail.com>
2022-03-23 19:27:41 +0530

    updated `fullAppDisplay` to source album info 1.1.81 onwards
    Again, hermes protocol gone, fixed album info

--- Extensions/fullAppDisplay.js
+++ Extensions/fullAppDisplay.js
@@ -341,9 +341,13 @@ body.video-full-screen.video-full-screen--hide-ui {
 
         async getAlbumDate(uri) {
             const id = uri.replace("spotify:album:", "");
-            const albumInfo = await Spicetify.CosmosAsync.get(`hm://album/v1/album-app/album/${id}/desktop`);
+            // const albumInfo = await Spicetify.CosmosAsync.get(`hm://album/v1/album-app/album/${id}/desktop`);
+            // const albumDate = new Date(albumInfo.year, (albumInfo.month || 1) - 1, albumInfo.day || 0);
+            // hermes protocol deprecated 1.1.81 onwards
 
-            const albumDate = new Date(albumInfo.year, (albumInfo.month || 1) - 1, albumInfo.day || 0);
+            const albumInfo = await Spicetify.CosmosAsync.get(`https://api.spotify.com/v1/albums/${id}`);
+
+            const albumDate = new Date(albumInfo.release_date);
             const recentDate = new Date();
             recentDate.setMonth(recentDate.getMonth() - 6);
             return albumDate.toLocaleString("default", albumDate > recentDate ? { year: "numeric", month: "short" } : { year: "numeric" });
