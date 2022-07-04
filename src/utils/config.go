a557cdbf1f2a1e223af57d811946a86bc664d254
ririxi <mail@ririxi.dev>
2022-07-04 03:01:09 +0200

    fix(config): finding spotify on gentoo (#1804)

--- src/utils/config.go
+++ src/utils/config.go
@@ -294,6 +294,7 @@ func linuxApp() string {
 
 	potentialList := []string{
 		"/opt/spotify/",
+		"/opt/spotify/spotify-client/",
 		"/usr/share/spotify/",
 		"/usr/libexec/spotify/",
 		"/var/lib/flatpak/app/com.spotify.Client/x86_64/stable/active/files/extra/share/spotify/",
