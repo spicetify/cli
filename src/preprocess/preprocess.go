711c4578d1b78f5a52a83494f111841ed76cfc7c
Nam Anh <phamnamanh25@gmail.com>
2022-09-29 21:19:35 +0700

    feat(preprocess): add support for error message bubble (#1963)

--- src/preprocess/preprocess.go
+++ src/preprocess/preprocess.go
@@ -268,7 +268,7 @@ func exposeAPIs_main(input string) string {
 	utils.Replace(
 		&input,
 		`,(\w+)=(\(\w+=\w+\.dispatch)`,
-		`;globalThis.Spicetify.showNotification=(message)=>${1}({message});const ${1}=${2}`)
+		`;globalThis.Spicetify.showNotification=(message,isError=false)=>${1}({message,feedbackType:isError?"ERROR":"NOTICE"});const ${1}=${2}`)
 
 	// Remove list of exclusive shows
 	utils.Replace(
