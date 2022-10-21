f594ac4b74877750f2b9ef3e572a6d3e200a96df
A1pha1 <janrobinkautz@gmail.com>
2022-10-21 20:16:22 +0200

    Added Japanese-lyrics conversion for lyricsplus

--- CustomApps/lyrics-plus/OptionsMenu.js
+++ CustomApps/lyrics-plus/OptionsMenu.js
@@ -85,6 +85,70 @@ const OptionsMenu = react.memo(({ options, onSelect, selected, defaultValue, bol
     );
 });
 
+const TranslationMenu = react.memo(({ showTranslationButton }) => {
+    if(!showTranslationButton)
+        return null;
+
+    return react.createElement(
+        Spicetify.ReactComponent.ContextMenu,
+        {
+            menu: react.createElement(
+                Spicetify.ReactComponent.Menu,
+                {},
+                react.createElement("h3", null, " Translations"),
+                react.createElement(OptionList, {
+                    items: [
+                        {
+                            desc: "Mode",
+                            key: "translation-mode",
+                            type: ConfigSelection,
+                            options: {
+                                furigana: "Furigana", 
+                                romaji:"Romaji", 
+                                hiragana: "Hiragana", 
+                                katakana: "Katakana"
+                            },
+                            renderInline: true,
+                        },
+                        {
+                            desc: "Translate",
+                            key: "translate",
+                            type: ConfigSlider,
+                            trigger: "click",
+                            action: "toggle",
+                            renderInline: true,
+                        }
+                    ],
+                    onChange: (name, value) => {
+                        CONFIG.visual[name] = value;
+                        localStorage.setItem(`${APP_NAME}:visual:${name}`, value);
+                        lyricContainerUpdate && lyricContainerUpdate();
+                    },
+                })
+            ),
+            trigger: "click",
+            action: "toggle",
+            renderInline: true,
+        },
+        react.createElement(
+            "button",
+            {
+                className: "lyrics-config-button",
+            },
+            react.createElement(
+                "p1",
+                {
+                    width: 16,
+                    height: 16,
+                    viewBox: "0 0 16 10.3",
+                    fill: "currentColor",
+                },
+                "あ"
+            )
+        )
+    );
+});
+
 const AdjustmentsMenu = react.memo(({ mode }) => {
     return react.createElement(
         Spicetify.ReactComponent.ContextMenu,
