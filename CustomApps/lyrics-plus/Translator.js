d581a0c466a22f8214385b5604ec92bf4492805f
A1pha1 <janrobinkautz@gmail.com>
2022-10-23 10:11:25 +0200

    fix: fetch dicts via jsdeliver if not found

--- CustomApps/lyrics-plus/Translator.js
+++ CustomApps/lyrics-plus/Translator.js
@@ -6047,17 +6047,27 @@
         )(29);
     });
 
+const dict1_url = "https://cdn.jsdelivr.net/gh/41pha1/spicetify-romaji-lyrics@main/marketplace/dict_part_1.js";
+const dict2_url = "https://cdn.jsdelivr.net/gh/41pha1/spicetify-romaji-lyrics@main/marketplace/dict_part_2.js";
+
 class Translator {
     constructor() {
         this.kuroshiro = new Kuroshiro.default();
         this.missingdicts = true;
         this.initializing = false;
+        this.downloadingdicts = false;
         this.finished = false;
         this.init();
     }
 
     init() {
         if (typeof base_dat_gz === "undefined" || typeof tid_pos_dat_gz === "undefined") {
+            if(!this.downloadingdicts)
+            {
+                setTimeout(this.include_external.bind(this), 0, dict1_url);
+                setTimeout(this.include_external.bind(this), 0, dict2_url);
+                this.downloadingdicts = true;
+            }
             setTimeout(this.init.bind(this), 100);
             return;
         }
@@ -6067,6 +6077,15 @@ class Translator {
             this.initializing = false;
             this.finished = true;
         });
+    }
+    
+    include_external(url) {
+        var s = document.createElement("script");
+        s.setAttribute("type", "text/javascript");
+        s.setAttribute("src", url);
+        var nodes = document.getElementsByTagName("*");
+        var node = nodes[nodes.length - 1].parentNode;
+        node.appendChild(s);
     }
 
     async romajifyText(text, target = "romaji", mode = "spaced") {
