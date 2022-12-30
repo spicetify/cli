9d3ce2fbeebbbd51b30443ad5d5f79b27fc9206a
Nam Anh <phamnamanh25@gmail.com>
2022-12-30 17:20:09 +0700

    refactor: move function to `Utils`

--- CustomApps/lyrics-plus/Pages.js
+++ CustomApps/lyrics-plus/Pages.js
@@ -120,39 +120,7 @@ const SyncedLyricsPage = react.memo(({ lyrics = [], provider, copyright, isKara 
 		offset += -(activeLineEle.current.offsetTop + activeLineEle.current.clientHeight / 2);
 	}
 
-	const rawLyrics = lyrics
-		.map(line => {
-			// Process converted Japanese lyrics
-			if (line.text.props?.children) {
-				line.rawText = line.text.props.children
-					.map(child => {
-						if (typeof child === "string") {
-							return child;
-						} else if (child.props?.children) {
-							return child.props?.children[0];
-						}
-					})
-					.join("");
-			}
-			if (!line.startTime) return line.text;
-			let startTimeString = "";
-
-			// Convert milliseconds to mm:ss format
-			if (!isNaN(line.startTime)) {
-				let minutes = Math.trunc(line.startTime / 60000),
-					seconds = ((line.startTime - minutes * 60000) / 1000).toFixed(2);
-
-				if (minutes < 10) minutes = "0" + minutes;
-				if (seconds < 10) seconds = "0" + seconds;
-
-				startTimeString = `${minutes}:${seconds}`;
-			} else {
-				startTimeString = line.startTime.toString();
-			}
-
-			return `[${startTimeString}]${line.rawText || line.text}`;
-		})
-		.join("\n");
+	const rawLyrics = Utils.convertParsedToLRC(lyrics);
 
 	return react.createElement(
 		"div",
@@ -400,39 +368,7 @@ const SyncedExpandedLyricsPage = react.memo(({ lyrics, provider, copyright, isKa
 		}
 	}
 
-	const rawLyrics = lyrics
-		.map(line => {
-			// Process converted Japanese lyrics
-			if (line.text.props?.children) {
-				line.rawText = line.text.props.children
-					.map(child => {
-						if (typeof child === "string") {
-							return child;
-						} else if (child.props?.children) {
-							return child.props?.children[0];
-						}
-					})
-					.join("");
-			}
-			if (!line.startTime) return line.text;
-			let startTimeString = "";
-
-			// Convert milliseconds to mm:ss format
-			if (!isNaN(line.startTime)) {
-				let minutes = Math.trunc(line.startTime / 60000),
-					seconds = ((line.startTime - minutes * 60000) / 1000).toFixed(2);
-
-				if (minutes < 10) minutes = "0" + minutes;
-				if (seconds < 10) seconds = "0" + seconds;
-
-				startTimeString = `${minutes}:${seconds}`;
-			} else {
-				startTimeString = line.startTime.toString();
-			}
-
-			return `[${startTimeString}]${line.rawText || line.text}`;
-		})
-		.join("\n");
+	const rawLyrics = Utils.convertParsedToLRC(lyrics);
 
 	useEffect(() => {
 		if (activeLineRef.current && (!intialScroll[0] || isInViewport(activeLineRef.current))) {
