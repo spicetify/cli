9e98d750d49f46ef4b3135c1d4b4ee0cd6a94d05
FivePixels <dylan.bolger00@gmail.com>
2023-01-11 00:33:46 -0600

    feat(installer): powershell logging

--- install.ps1
+++ install.ps1
@@ -10,9 +10,34 @@ if ($v) {
     $version = $v
 }
 
+$log_file_dir = "$sp_dir\install.log"
+
+# Function to log output to file.
+Function Log {
+  Param([String] $ActionText, [String[]] $Texts, [Boolean[]] $Emphasized, [Boolean] $NewLine)
+  if (-not (Test-Path $log_file_dir)) {
+    New-Item -Path $log_file_dir -ItemType File -Force | Out-Null
+  }
+  if (-not ([String]::IsNullOrEmpty($ActionText))) {
+    $FormattedActionText = "{0, -15}" -f $ActionText
+    Write-Part $FormattedActionText
+  }
+  $LogText = $FormattedActionText
+  for ($i=0; $i -lt $Texts.Length -AND $Texts.Length -eq $Emphasized.Length; $i++) {
+    if ($Emphasized.Get($i)) {
+      Write-Emphasized $Texts.Get($i)
+    } else {
+      Write-Part $Texts.Get($i)
+    }
+    $LogText = $LogText + $Texts.Get($i)
+  }
+  $LogText = "[{0}] {1}" -f (Get-Date -f "HH:mm:ss yyyy-MM-dd"), $LogText
+  Add-content $log_file_dir -value $LogText -NoNewline
+}
+
 # Helper functions for pretty terminal output.
-function Write-Part ([string] $Text) {
-  Write-Host $Text -NoNewline
+function Write-Part ([String] $Text) {
+  Write-Host $Text -Nonewline
 }
 
 function Write-Emphasized ([string] $Text) {
@@ -22,13 +47,14 @@ function Write-Emphasized ([string] $Text) {
 function Write-Done {
   Write-Host " > " -NoNewline
   Write-Host "OK" -ForegroundColor "Green"
+  Add-Content $log_file_dir -value " > OK"
 }
 
 function RemoveOldPath {
   $oldsp_dir = "${HOME}\spicetify-cli"
   $isinpath = $paths -contains $oldsp_dir -or $paths -contains "${oldsp_dir}\"
   if ($isinpath) {
-    Write-Part "REMOVING       "; Write-Emphasized $oldsp_dir; Write-Part " from Path"
+    Log "REMOVING" -Texts $oldsp_dir, " from Path" -Emphasized $true, $false
 
     $replacedpath = $path.replace(";$oldsp_dir", "")
     [Environment]::SetEnvironmentVariable("PATH", $replacedpath, $user)
@@ -41,10 +67,10 @@ function MigrateCfgFolder {
   $oldsp_dircontent = "${HOME}\spicetify-cli\*"
   $oldsp_dir = "${HOME}\spicetify-cli"
   if (Test-Path -Path $oldsp_dir) {
-    Write-Part "MIGRATING      "; Write-Emphasized $oldsp_dir; Write-Part " into ";  Write-Emphasized $sp_dir
+    Log "MIGRATING" -Texts $oldsp_dir, " into", $sp_dir -Emphasized $true, $false, $true
     Copy-item -Force -Recurse $oldsp_dircontent -Destination $sp_dir
     Write-Done
-    Write-Part "REMOVING       "; Write-Emphasized $oldsp_dir
+    Log "REMOVING" -Texts $oldsp_dir -Emphasized $true
     Remove-Item -LiteralPath $oldsp_dir -Force -Recurse
     Write-Done
   }
@@ -56,23 +82,22 @@ if ($PSVersionTable.PSVersion.Major -gt $PSMinVersion) {
   # Enable TLS 1.2 since it is required for connections to GitHub.
   [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
+  # Create %localappdata%\spicetify directory if it doesn't already exist
+  $sp_dir = "$env:LOCALAPPDATA\spicetify"
+  if (-not (Test-Path $sp_dir)) {
+    Log "MAKING FOLDER" -Texts $sp_dir -Emphasized $true
+    Write-Done
+  }
+
   if (-not $version) {
     # Determine latest Spicetify release via GitHub API.
     $latest_release_uri =
     "https://api.github.com/repos/spicetify/spicetify-cli/releases/latest"
-    Write-Part "DOWNLOADING    "; Write-Emphasized $latest_release_uri
+    Log "DOWNLOADING" -Texts $latest_release_uri -Emphasized $true
     $latest_release_json = Invoke-WebRequest -Uri $latest_release_uri -UseBasicParsing
     Write-Done
 
     $version = ($latest_release_json | ConvertFrom-Json).tag_name -replace "v", ""
-  }
-
-  # Create %localappdata%\spicetify directory if it doesn't already exist
-  $sp_dir = "$env:LOCALAPPDATA\spicetify"
-  if (-not (Test-Path $sp_dir)) {
-    Write-Part "MAKING FOLDER  "; Write-Emphasized $sp_dir
-    New-Item -Path $sp_dir -ItemType Directory | Out-Null
-    Write-Done
   }
 
   # Migrate old spicetify folder to new location.
@@ -83,19 +108,18 @@ if ($PSVersionTable.PSVersion.Major -gt $PSMinVersion) {
   $zip_file = "${sp_dir}\spicetify-${version}-windows-${architecture}.zip"
   $download_uri = "https://github.com/spicetify/spicetify-cli/releases/download/" +
   "v${version}/spicetify-${version}-windows-${architecture}.zip"
-  Write-Part "DOWNLOADING    "; Write-Emphasized $download_uri
+  Log "DOWNLOADING" -Texts $download_uri -Emphasized $true
   Invoke-WebRequest -Uri $download_uri -UseBasicParsing -OutFile $zip_file
   Write-Done
 
   # Extract spicetify.exe and assets from .zip file.
-  Write-Part "EXTRACTING     "; Write-Emphasized $zip_file
-  Write-Part " into "; Write-Emphasized ${sp_dir};
+  Log "EXTRACTING" -Texts $zip_file, " into ", ${sp_dir} -Emphasized $true, $false, $true
   # Using -Force to overwrite spicetify.exe and assets if it already exists
   Expand-Archive -Path $zip_file -DestinationPath $sp_dir -Force
   Write-Done
 
   # Remove .zip file.
-  Write-Part "REMOVING       "; Write-Emphasized $zip_file
+  Log "REMOVING" -Texts $zip_file -Emphasized $true
   Remove-Item -Path $zip_file
   Write-Done
 
@@ -112,8 +136,7 @@ if ($PSVersionTable.PSVersion.Major -gt $PSMinVersion) {
 
   # Add Spicetify dir to PATH if it hasn't been added already.
   if (-not $is_in_path) {
-    Write-Part "ADDING         "; Write-Emphasized $sp_dir; Write-Part " to the "
-    Write-Emphasized "PATH"; Write-Part " environment variable..."
+    Log "ADDING" -Texts $sp_dir, " to the ", "PATH", " environment variable..." -Emphasized $true, $false, $true, $false
     [Environment]::SetEnvironmentVariable("PATH", "${path};${sp_dir}", $user)
     # Add Spicetify to the PATH variable of the current terminal session
     # so `spicetify` can be used immediately without restarting the terminal.
@@ -121,9 +144,9 @@ if ($PSVersionTable.PSVersion.Major -gt $PSMinVersion) {
     Write-Done
   }
 
-  Write-Part "spicetify-cli was installed successfully."; Write-Done
-  Write-Part "Run "; Write-Emphasized "spicetify --help"; Write-Host " to get started.`n"
+  Log -Texts "spicetify-cli was installed successfully." -Emphasized $false; Write-Done
+  Log -Texts "Run ", "spicetify --help", " to get started.`n" -Emphasized $false, $true, $false
 } else {
-  Write-Part "`nYour Powershell version is lesser than "; Write-Emphasized "$PSMinVersion";
-  Write-Part "`nPlease, update your Powershell downloading the "; Write-Emphasized "'Windows Management Framework'"; Write-Part " greater than "; Write-Emphasized "$PSMinVersion"
+  Log -Texts "`nYour Powershell version is lesser than ", "$PSMinVersion" -Emphasized $false, $true
+  Log -Texts "`nPlease, update your Powershell downloading the ", "'Windows Management Framework'", " greater than ", "$PSMinVersion" -Emphasized $false, $true, $false, $true
 }
