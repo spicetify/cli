fafc5951271e041b8a397ee437d20aebea8c3714
Raghavan <raghavanmarch2002@gmail.com>
2023-03-15 18:28:20 +0530

    Update globals.d.ts
    Co-authored-by: Nam Anh <phamnamanh25@gmail.com>

--- globals.d.ts
+++ globals.d.ts
@@ -367,7 +367,7 @@ declare namespace Spicetify {
          */
         type Icon = "album" | "artist" | "block" | "brightness" | "car" | "chart-down" | "chart-up" | "check" | "check-alt-fill" | "chevron-left" | "chevron-right" | "chromecast-disconnected" | "clock" | "collaborative" | "computer" | "copy" | "download" | "downloaded" | "edit" | "enhance" | "exclamation-circle" | "external-link" | "facebook" | "follow" | "fullscreen" | "gamepad" | "grid-view" | "heart" | "heart-active" | "instagram" | "laptop" | "library" | "list-view" | "location" | "locked" | "locked-active" | "lyrics" | "menu" | "minimize" | "minus" | "more" | "new-spotify-connect" | "offline" | "pause" | "phone" | "play" | "playlist" | "playlist-folder" | "plus-alt" | "plus2px" | "podcasts" | "projector" | "queue" | "repeat" | "repeat-once" | "search" | "search-active" | "shuffle" | "skip-back" | "skip-back15" | "skip-forward" | "skip-forward15" | "soundbetter" | "speaker" | "spotify" | "subtitles" | "tablet" | "ticket" | "twitter" | "visualizer" | "voice" | "volume" | "volume-off" | "volume-one-wave" | "volume-two-wave" | "watch" | "x";
         class Item {
-            constructor(name: string, isEnabled: boolean, onClick: (self: Item) => void, icon: Icon | string);
+            constructor(name: string, isEnabled: boolean, onClick: (self: Item) => void, icon?: Icon | string);
             name: string;
             isEnabled: boolean;
             /**
