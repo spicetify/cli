320783f112a446b3fe763b636e03ce19f4b14502
Jeong Hyeon <da864268@naver.com>
2023-03-27 13:21:50 +0900

    fix(popupLyrics): providers change fix (#2219)

--- Extensions/popupLyrics.js
+++ Extensions/popupLyrics.js
@@ -270,13 +270,13 @@ function PopupLyrics() {
 			musixmatch: {
 				on: boolLocalStorage("popup-lyrics:services:musixmatch:on"),
 				call: LyricProviders.fetchMusixmatch,
-				desc: `Fully compatible with Spotify. Requires a token that can be retrieved from the official Musixmatch app. Follow instructions on <a href="https://github.com/khanhas/spicetify-cli/wiki/Musixmatch-Token">spicetify Wiki</a>.`,
+				desc: `Fully compatible with Spotify. Requires a token that can be retrieved from the official Musixmatch app. Follow instructions on <a href="https://spicetify.app/docs/faq#sometimes-popup-lyrics-andor-lyrics-plus-seem-to-not-work">Spicetify Docs</a>.`,
 				token: LocalStorage.get("popup-lyrics:services:musixmatch:token") || "2005218b74f939209bda92cb633c7380612e14cb7fe92dcd6a780f"
 			},
 			spotify: {
 				on: boolLocalStorage("popup-lyrics:services:spotify:on"),
 				call: LyricProviders.fetchSpotify,
-				desc: `Lyrics officially provided by Spotify. Only available for some regions/countries' users (e.g., Japan, Vietnam, Thailand).`
+				desc: `Lyrics sourced from official Spotify API.`
 			}
 		},
 		servicesOrder: []
@@ -382,24 +382,21 @@ function PopupLyrics() {
 			uri: Player.data.track.uri
 		};
 
-		sharedData = { lyrics: [] };
-		let error = null;
-
 		for (let name of userConfigs.servicesOrder) {
 			const service = userConfigs.services[name];
 			if (!service.on) continue;
+			sharedData = { lyrics: [] };
 
 			try {
 				const data = await service.call(info);
 				console.log(data);
 				sharedData = data;
-				return;
+				if (!sharedData.error) {
+					return;
+				}
 			} catch (err) {
-				error = err;
+				sharedData = { error: "No lyrics" };
 			}
-		}
-		if (error || !sharedData.lyrics) {
-			sharedData = { error: "No lyrics" };
 		}
 	}
 
