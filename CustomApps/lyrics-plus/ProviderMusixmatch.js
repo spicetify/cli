bc2c3d2633826947922e5df78318281a4e4dbf48
kakkokari-gtyih <67428053+kakkokari-gtyih@users.noreply.github.com>
2023-04-07 19:30:27 +0900

    feat(lyrics-plus): add musixmatch word sync (#2248)

--- CustomApps/lyrics-plus/ProviderMusixmatch.js
+++ CustomApps/lyrics-plus/ProviderMusixmatch.js
@@ -45,6 +45,64 @@ const ProviderMusixmatch = (function () {
 		return body;
 	}
 
+	async function getKaraoke(body) {
+		const meta = body?.["matcher.track.get"]?.message?.body;
+		if (!meta) {
+			return null;
+		}
+
+		if (!meta.track.has_richsync || meta.track.instrumental) {
+			return null;
+		}
+
+		const baseURL = `https://apic-desktop.musixmatch.com/ws/1.1/track.richsync.get?format=json&subtitle_format=mxm&app_id=web-desktop-app-v1.0&`;
+
+		const params = {
+			subtitle_length: meta.track.track_length,
+			commontrack_id: meta.track.commontrack_id,
+			usertoken: CONFIG.providers.musixmatch.token
+		};
+
+		const finalURL =
+			baseURL +
+			Object.keys(params)
+				.map(key => key + "=" + encodeURIComponent(params[key]))
+				.join("&");
+
+		let result = await CosmosAsync.get(finalURL, null, headers);
+
+		if (result.message.header.status_code != 200) {
+			return null;
+		}
+
+		result = result.message.body;
+
+		const parsedKaraoke = JSON.parse(result.richsync.richsync_body).map(line => {
+			const startTime = line.ts * 1000;
+			const endTime = line.te * 1000;
+			const words = line.l;
+
+			const text = words.map((word, index, words) => {
+				const wordText = word.c;
+				const wordStartTime = word.o * 1000;
+				const nextWordStartTime = words[index + 1]?.o || null;
+
+				const time = nextWordStartTime ? nextWordStartTime - wordStartTime : endTime - (wordStartTime + startTime);
+
+				return {
+					word: wordText,
+					time
+				};
+			});
+			return {
+				startTime,
+				text
+			};
+		});
+
+		return parsedKaraoke;
+	}
+
 	function getSynced(body) {
 		const meta = body?.["matcher.track.get"]?.message?.body;
 		if (!meta) {
@@ -95,5 +153,5 @@ const ProviderMusixmatch = (function () {
 		return null;
 	}
 
-	return { findLyrics, getSynced, getUnsynced };
+	return { findLyrics, getKaraoke, getSynced, getUnsynced };
 })();
