46c620a6b60803640ff84e78d7326bc5d4976474
OhItsTom <22730962+ohitstom@users.noreply.github.com>
2023-05-06 15:41:00 +0100

    prettier

--- CustomApps/lyrics-plus/PlaybarButton.js
+++ CustomApps/lyrics-plus/PlaybarButton.js
@@ -37,7 +37,7 @@
 	});
 
 	if (Spicetify.Platform.History.location.pathname === "/lyrics-plus") button.active = true;
-	Spicetify.Platform.History.listen(location => button.active = location.pathname === "/lyrics-plus");
+	Spicetify.Platform.History.listen(location => (button.active = location.pathname === "/lyrics-plus"));
 
 	function setPlaybarButton() {
 		document.head.appendChild(style);
