a43d764b16435e22b5319bacdd24cf64763fbf08
kyrie25 <kyrie25@users.noreply.github.com>
2023-05-14 11:46:57 +0000

    style: prettify code

--- CustomApps/new-releases/Card.js
+++ CustomApps/new-releases/Card.js
@@ -139,16 +139,16 @@ class Card extends react.Component {
 										"svg",
 										{
 											width: "16",
-										height: "16",
-										viewBox: "0 0 16 16",
-										xmlns: "http://www.w3.org/2000/svg",
-										className: "main-card-closeButton-svg"
-									},
-									react.createElement("path", {
-										d: "M1.47 1.47a.75.75 0 0 1 1.06 0L8 6.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L9.06 8l5.47 5.47a.75.75 0 1 1-1.06 1.06L8 9.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L6.94 8 1.47 2.53a.75.75 0 0 1 0-1.06z",
-										fill: "var(--spice-text)",
-										fillRule: "evenodd"
-									})
+											height: "16",
+											viewBox: "0 0 16 16",
+											xmlns: "http://www.w3.org/2000/svg",
+											className: "main-card-closeButton-svg"
+										},
+										react.createElement("path", {
+											d: "M1.47 1.47a.75.75 0 0 1 1.06 0L8 6.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L9.06 8l5.47 5.47a.75.75 0 1 1-1.06 1.06L8 9.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L6.94 8 1.47 2.53a.75.75 0 0 1 0-1.06z",
+											fill: "var(--spice-text)",
+											fillRule: "evenodd"
+										})
 									)
 								)
 							)
