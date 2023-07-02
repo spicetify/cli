04cfe1de12632d68076024eacb0ec057c313ca9f
ririxi <hey@ririxi.dev>
2023-07-02 23:56:10 +0200

    chore: stop duplicating the message

--- install.ps1
+++ install.ps1
@@ -87,7 +87,7 @@ function Move-ConfigFolder {
 #region Main
 if ($PSVersionTable.PSVersion.Major -ge $PSMinVersion) {
   if ([bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -contains 'S-1-5-32-544')) {
-    Write-Log -ActionText "The script was ran as Administrator. Exiting..."
+    Write-Log -ActionText "The script was ran as Administrator. Exiting..." | Out-Null
     throw "The script was run as Administrator"
   }
 
