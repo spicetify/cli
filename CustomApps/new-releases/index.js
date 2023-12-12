02ada2b883ff0ceab64a7e3e1fba4c36f54a87de
OhItsTom <22730962+ohitstom@users.noreply.github.com>
2023-12-12 14:14:53 +0000

    use pop instead of slice

--- CustomApps/new-releases/index.js
+++ CustomApps/new-releases/index.js
@@ -121,7 +121,7 @@ class Grid extends react.Component {
 			case "undo":
 				if (!dismissed[0]) Spicetify.showNotification("Nothing to undo", true);
 				else Spicetify.showNotification("Undone dismissal");
-				dismissed = id ? dismissed.filter(item => item !== id) : dismissed.slice(0, -1);
+				dismissed = id ? dismissed.filter(item => item !== id) : dismissed.pop();
 				break;
 			default:
 				dismissed.push(id);
