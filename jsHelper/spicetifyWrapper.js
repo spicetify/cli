8315bbe82422c218d48ee26456c336f59bb491af
ririxi <dev@ririxi.dev>
2024-02-16 15:37:40 +0100

    fix: variable name for `internalURL` in `useProxy`

--- jsHelper/spicetifyWrapper.js
+++ jsHelper/spicetifyWrapper.js
@@ -337,7 +337,7 @@ window.Spicetify = {
 				const isSpotifyAPI = exposedURL.hostname === "api.spotify.com";
 				const isSpClientAPI = exposedURL.hostname.includes("spclient") && exposedURL.hostname.includes(".spotify.com");
 				const isInternalURL = exposedURL.protocol === "sp";
-				const useProxy = !internalUrl && !isSpotifyAPI;
+				const useProxy = !isInternalURL && !isSpotifyAPI;
 				const method = mappedMethods[prop.toLowerCase()] || prop.toLowerCase();
 
 				const options = {
