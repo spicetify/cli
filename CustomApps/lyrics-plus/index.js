552f3b625733ff54bb7e27d5b70cc785903f1b45
ririxi <dev@ririxi.dev>
2024-02-25 18:48:05 +0100

    style: use html tags

--- CustomApps/lyrics-plus/index.js
+++ CustomApps/lyrics-plus/index.js
@@ -73,7 +73,7 @@ const CONFIG = {
 		},
 		genius: {
 			on: getConfig("lyrics-plus:provider:genius:on"),
-			desc: "Provide unsynced lyrics with insights from artists themselves. Genius is disabled on `1.2.31` and higher and cannot be used as an provider.",
+			desc: "Provide unsynced lyrics with insights from artists themselves. Genius is disabled on <code>1.2.31</code> and higher and cannot be used as an provider.",
 			modes: [GENIUS]
 		},
 		local: {
