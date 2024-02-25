1d61bf0ee691c79bb6f13d4961a9be259372a9f0
ririxi <dev@ririxi.dev>
2024-02-25 22:52:44 +0100

    chore: change desc

--- CustomApps/lyrics-plus/index.js
+++ CustomApps/lyrics-plus/index.js
@@ -73,7 +73,7 @@ const CONFIG = {
 		},
 		genius: {
 			on: getConfig("lyrics-plus:provider:genius:on"),
-			desc: "Provide unsynced lyrics with insights from artists themselves. Genius is disabled on <code>1.2.31</code> and higher and cannot be used as an provider.",
+			desc: "Provide unsynced lyrics with insights from artists themselves. Genius is disabled and higher and cannot be used as a provider on <code>1.2.31</code>.",
 			modes: [GENIUS]
 		},
 		local: {
