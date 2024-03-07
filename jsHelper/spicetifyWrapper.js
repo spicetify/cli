30b9b8697259ca276bff4c0367f8c2656ad9dc65
ririxi <dev@ririxi.dev>
2024-03-07 23:51:07 +0100

    feat: parse more types of responses

--- jsHelper/spicetifyWrapper.js
+++ jsHelper/spicetifyWrapper.js
@@ -374,11 +374,18 @@ window.Spicetify = {
 				Object.assign(options.headers, injectedHeaders);
 
 				try {
-					return fetch(finalURL, options).then(res =>
-						res.json().catch(() => {
-							return { status: res.status };
-						})
-					);
+					return fetch(finalURL, options).then(res => {
+						if (!res.ok) return { code: res.status, error: res.statusText, message: "Failed to fetch", stack: undefined };
+						try {
+							return res.clone().json();
+						} catch {
+							try {
+								return res.clone().blob();
+							} catch {
+								return res.clone().text();
+							}
+						}
+					});
 				} catch (e) {
 					console.error(e);
 				}
