0891b88f0ff4be853635d1a2b88c22e5bb15eac7
ririxi <dev@ririxi.dev>
2024-03-11 12:13:00 +0100

    fix(wrapper): use typeof for checking for json

--- jsHelper/spicetifyWrapper.js
+++ jsHelper/spicetifyWrapper.js
@@ -343,21 +343,12 @@ window.Spicetify = {
 					timeout: 1000 * 15
 				};
 
-				function isJson(str) {
-					try {
-						JSON.parse(str);
-					} catch {
-						return false;
-					}
-					return true;
-				}
-
 				let finalURL = urlObj.toString();
 				if (body) {
 					if (method === "get") {
 						const params = new URLSearchParams(body);
 						finalURL += `?${params.toString()}`;
-					} else options.body = isJson ? JSON.stringify(body) : body;
+					} else options.body = Array.isArray(body) && typeof body === "object" ? JSON.stringify(body) : body;
 				}
 				if (shouldUseCORSProxy) finalURL = `${corsProxyURL}/${finalURL}`;
 
