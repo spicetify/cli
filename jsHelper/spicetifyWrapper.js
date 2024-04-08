0aca1defa6217e77dfa91cc07e4dd3737aeeb5f5
ririxi <dev@ririxi.dev>
2024-04-08 23:45:46 +0200

    fix(wrapper): adapt strings to find `Cards.CardImage`

--- jsHelper/spicetifyWrapper.js
+++ jsHelper/spicetifyWrapper.js
@@ -265,7 +265,9 @@ window.Spicetify = {
 					"StoreProvider",
 					"PlatformProvider",
 					"Dropdown",
-					"MenuSubMenuItem"
+					"MenuSubMenuItem",
+					"Navigation",
+					"ScrollableContainer"
 				])
 			},
 			{
@@ -557,7 +559,12 @@ window.Spicetify = {
 					m => m.toString().includes("?highlight") && m.toString().includes("headerText") && m.toString().includes("imageContainer")
 				),
 				Hero: functionModules.find(m => m?.toString().includes('"herocard-click-handler"')),
-				CardImage: functionModules.find(m => m.toString().includes("isHero") && m.toString().includes("withWaves")),
+				CardImage: functionModules.find(
+					m =>
+						m.toString().includes("isHero") &&
+						(m.toString().includes("withWaves") || m.toString().includes("isCircular")) &&
+						m.toString().includes("imageWrapper")
+				),
 				...Object.fromEntries(cards)
 			},
 			Router: functionModules.find(m => m.toString().includes("navigationType") && m.toString().includes("static")),
