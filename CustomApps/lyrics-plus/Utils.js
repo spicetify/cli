78d1a180ab7e1cb60a408edab4b9dec87303b47c
ririxi <dev@ririxi.dev>
2024-04-25 00:58:22 +0200

    fix: add optional chaining

--- CustomApps/lyrics-plus/Utils.js
+++ CustomApps/lyrics-plus/Utils.js
@@ -133,7 +133,7 @@ const Utils = {
 		return ((simpPercentage - tradPercentage + 1) / 2) * 100 >= CONFIG.visual["hans-detect-threshold"] ? "zh-hans" : "zh-hant";
 	},
 	processTranslatedLyrics(result, lyricsToTranslate, { state, stateName }) {
-		const translatedLines = result.split("\n").filter(line => line !== "" && typeof line === "string");
+		const translatedLines = result.split("\n").filter(line => line !== "");
 
 		state[stateName] = [];
 
@@ -146,7 +146,7 @@ const Utils = {
 	rubyTextToReact(s) {
 		const react = Spicetify.React;
 
-		const rubyElems = s.split("<ruby>");
+		const rubyElems = s?.split("<ruby>");
 		const reactChildren = [];
 
 		reactChildren.push(rubyElems[0]);
