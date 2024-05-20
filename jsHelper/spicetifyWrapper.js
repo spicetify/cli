488d5359946e649f7c6887ea7e66ac1a444d8e09
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2024-05-20 13:36:00 +0100

    fix: missing wrapper replacement

--- jsHelper/spicetifyWrapper.js
+++ jsHelper/spicetifyWrapper.js
@@ -2391,7 +2391,7 @@ Spicetify.Playbar = (() => {
 	// Fetch latest version from GitHub
 	try {
 		let changelog;
-		const res = await fetch("https://api.github.com/repos/spicetify/spicetify-cli/releases/latest");
+		const res = await fetch("https://api.github.com/repos/spicetify/cli/releases/latest");
 		const { tag_name, html_url, body } = await res.json();
 		const semver = tag_name.slice(1);
 		const changelogRawDataOld = body.match(/## What's Changed([\s\S]*?)\r\n\r/)?.[1];
