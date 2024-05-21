b4b90d41219ff8cd42b7e2b3a65a04e2a81be97f
ririxi <dev@ririxi.dev>
2024-05-21 23:03:43 +0300

    fix(wrapper): exclude string to find module on `1.2.38`

--- jsHelper/spicetifyWrapper.js
+++ jsHelper/spicetifyWrapper.js
@@ -698,7 +698,10 @@ window.Spicetify = {
 
 	// classnames
 	// https://github.com/JedWatson/classnames/
-	const classnamesChunk = chunks.find(([_, value]) => value.toString().includes("[native code]") && !value.toString().includes("<anonymous>"));
+	const classnamesChunk = chunks.find(
+		([_, value]) =>
+			value.toString().includes("[native code]") && !value.toString().includes("<anonymous>") && !value.toString().includes("Super expression")
+	);
 	if (classnamesChunk && !Spicetify.classnames) {
 		Spicetify.classnames = Object.values(require(classnamesChunk[0])).find(m => typeof m === "function");
 	}
