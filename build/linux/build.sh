c4b08f4162632298140c367ef4910086589a5b7e
em <dev@ririxi.dev>
2024-07-21 15:08:07 +0200

    refactor: release ci (#3111)

--- build/linux/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-version=$1
-arch=$2
-
-GOARCH=$arch go build -C ../../ -o build/linux/spicetify -ldflags "-X main.version=$version"
--- build/macos/build.sh
+++ build/macos/build.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env sh
 set -e
 
-version=$1
-
-mkdir Volume
+mkdir Volume && mkdir -p bin
 osacompile -x -o Volume/Spicetify.app main.applescript
 rm Volume/Spicetify.app/Contents/Resources/applet.icns
 cp installer/AppIcon.icns Volume/Spicetify.app/Contents/Resources/AppIcon.icns
+lipo -create -output bin/spicetify ../../artifacts/spicetify-amd64 ../../artifacts/spicetify-arm64 && echo "Built universal binary"
 
-GOARCH="amd64" go build -C ../../ -o build/macos/spicetify-amd64 -ldflags "-X main.version=$version"
-GOARCH="arm64" go build -C ../../ -o build/macos/spicetify-arm64 -ldflags "-X main.version=$version"
 mkdir -p Volume/Spicetify.app/Contents/MacOS/bin
-lipo -create -output Volume/Spicetify.app/Contents/MacOS/bin/spicetify spicetify-amd64 spicetify-arm64
-
+cp bin/spicetify Volume/Spicetify.app/Contents/MacOS/bin/spicetify
 plutil -replace CFBundleName -string "Spicetify" Volume/Spicetify.app/Contents/Info.plist
 plutil -replace CFBundleIconFile -string AppIcon.icns Volume/Spicetify.app/Contents/Info.plist
 plutil -replace CFBundleURLTypes -xml '<array><dict><key>CFBundleURLName</key><string>Spicetify</string><key>CFBundleURLSchemes</key><array><string>spicetify</string></array></dict></array>' Volume/Spicetify.app/Contents/Info.plist
