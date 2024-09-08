8cd5671e12cf16d6d3bbe90395856942d55b3c17
OhItsTom <22730962+ohitstom@users.noreply.github.com>
2024-09-08 19:06:39 +0100

    requested changes

--- src/apply/apply.go
+++ src/apply/apply.go
@@ -31,6 +31,7 @@ func AdditionalOptions(appsFolderPath string, flags Flag) {
 		filepath.Join(appsFolderPath, "xpui", "xpui.js"):                insertCustomApp,
 		filepath.Join(appsFolderPath, "xpui", "vendor~xpui.js"):         insertExpFeatures,
 		filepath.Join(appsFolderPath, "xpui", "home-v2.js"):             insertHomeConfig,
+		filepath.Join(appsFolderPath, "xpui", "xpui.js"):                insertHomeConfig,
 		filepath.Join(appsFolderPath, "xpui", "xpui-desktop-modals.js"): insertVersionInfo,
 	}
 
@@ -352,6 +353,14 @@ func insertHomeConfig(jsPath string, flags Flag) {
 		utils.ReplaceOnce(
 			&content,
 			`(createDesktopHomeFeatureActivationShelfEventFactory.*?)([\w\.]+)(\.map)`,
+			func(submatches ...string) string {
+				return fmt.Sprintf("%sSpicetifyHomeConfig.arrange(%s)%s", submatches[1], submatches[2], submatches[3])
+			})
+
+		// >= 1.2.45
+		utils.ReplaceOnce(
+			&content,
+			`(&&"HomeShortsSectionData".*\],)([a-zA-Z])(\}\)\()`,
 			func(submatches ...string) string {
 				return fmt.Sprintf("%sSpicetifyHomeConfig.arrange(%s)%s", submatches[1], submatches[2], submatches[3])
 			})
